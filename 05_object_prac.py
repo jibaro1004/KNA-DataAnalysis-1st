335132b0102a5fd7e7459043ac01ec7077627bcc
jibaro1004 <jibaro1004@gmail.com>
2026-07-15 17:43:31 +0900

    study: 산술연산자

---
+++ 05_object_prac.py
@@ -0,0 +1,39 @@
+count = 5
+temp = 45
+str = "풍력발전기"
+is_ok = True
+print(count, temp, str, is_ok)
+
+
+print(type(count))
+print(type(temp))
+print(type(str))
+print(type(is_ok))
+
+
+print(type(100))  # 예상: int
+print(type(100.0))  # 예상: float!
+print(type("100"))  # 예상: str
+print(type(is_ok))  # 예상 : bool
+
+print(5 + 3)
+print("5" + "3")
+print("12" + "45")
+
+print(3 > 2)
+print(5 == 5)
+print(type(3 > 2))
+
+count = 3
+print(type(count))
+count = 3.0
+print(type(count))
+count = "3"
+print(type(count))
+
+device_temp = 3.4
+check_count = 4.55
+device_name = "배고픈 설비"
+is_normal = False
+
+print(5 > 3)
